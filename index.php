4695324c3c7f2518b2552a185370a346281f2a80
Misha Vinokur <misha@Mishas-MacBook-Pro.local>
2021-05-14 14:59:47 -0400

    removed erorr reporting

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 // Unless explicitly set everything is a 404 (will need a dynamic page route)
 http_response_code(404);
 
